92b9283226ea813f9f9de3ae4bb18005d466caf6
Hermes (agora) <hermes.frangoudis@agora.io>
2024-05-02 18:13:11 -0400

    update to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { JoinModalComponent } from './join-modal/join-modal.component';
+import { RemoteStreamComponent } from './remote-stream/remote-stream.component';
+import { LocalStreamComponent } from './local-stream/local-stream.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ 
+    CommonModule, 
+    RouterOutlet, 
+    JoinModalComponent, 
+    RemoteStreamComponent, 
+    LocalStreamComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular RTC';
+
+  @ViewChild('remoteStreamsContainer') remoteStreamsComponent!: RemoteStreamComponent;
+  title = 'agora-angular-demo';
+
+  isJoinModalVisible = true;
+  isLocalStreamVisible = false;
+
+  handleJoinChannel() {
+    this.isJoinModalVisible = false;
+    this.isLocalStreamVisible = true;
+  }
+
+  handleLeaveChannel() {
+    this.isLocalStreamVisible = false;
+    this.isJoinModalVisible = true;
+    this.remoteStreamsComponent.clearRemoteUsers()
+  }
 }
